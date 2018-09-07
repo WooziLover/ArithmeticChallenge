1de27279314893a38502041825d37342a5954b7d
Tristyn J Lai <tl00456@srcs.loc>
2018-09-07 12:00:01 -0700

    Initial commit

---
+++ app/src/test/java/com/example/tl00456/arithemticchallenge/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.tl00456.arithemticchallenge;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}

5cb998019eba985ff7f911683849a4931647f556
tl00456 <oohty15@gmail.com>
2018-09-12 14:36:22 -0700

    Draft of Lab 3

--- app/src/main/java/com/example/tl00456/arithemticchallenge/MainActivity.java
+++ app/src/main/java/com/example/tl00456/arithemticchallenge/MainActivity.java
@@ -1,7 +1,20 @@
 package com.example.tl00456.arithemticchallenge;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ImageButton;
+import android.widget.Button;
+import android.content.Intent;
+
+/***********************************************************************
+ * Description: This is the controller of the main activity to react
+ *              user activities.
+ *
+ * @author Tristyn Lai
+ * @version September 12, 2018 2:00
+ ***********************************************************************/
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +22,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listenForUserInput();
     }
 
-    public void aMethodToDemoAPush(){
-        //does nothing
+    private void listenForUserInput(){
+        // sets asset ids for use in setOnClickListener
+        final ImageButton[] operation = {
+                findViewById(R.id.radioButton4_Addition),
+                findViewById(R.id.radioButton3_Subtraction),
+                findViewById(R.id.radioButton3_Multiplication),
+                findViewById(R.id.radioButton4_Division)};
+        final Button instructionButton = findViewById(R.id.button6_Instructions);
+        final Button exitButton = findViewById(R.id.button5_Exit);
+
+        // sets operation strings for use in intent put
+        final String operationName[] = {"addition", "subtraction", "multiplication", "division"};
+
+
+        // listens for clicks to pass intent
+        for(int i = 0; i < operation.length; i++) {
+            final int operationNum = i; //declared final to pass into inner class
+            operation[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), GameActivity.class);
+                    intent.putExtra("Operation", operationName[operationNum]);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
     }
 }

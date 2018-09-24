978d15d3e9f378104f8df8779d303dc5e8a23831
WooziLover <oohty15@gmail.com>
2018-09-24 00:34:12 -0700

    Lab 4 Rough Draft 2

--- app/src/main/java/com/example/tl00456/arithemticchallenge/Multiplication.java
+++ app/src/main/java/com/example/tl00456/arithemticchallenge/Multiplication.java
@@ -4,34 +4,34 @@ public class Multiplication extends ArithmeticChallenge {
     int firstnum, secondnum;
     String operator;
 
-    public Multiplication(int TOTAL_LEVELS, int TURNS_PER_LEVEL){
+    public Multiplication(int TOTAL_LEVELS, int TURNS_PER_LEVEL) {
         super(TOTAL_LEVELS, TURNS_PER_LEVEL);
-        firstnum = (int)(12 * Math.random());
-        secondnum = (int)(12 * Math.random());
+        firstnum = (int) (12 * Math.random());
+        secondnum = (int) (12 * Math.random());
     }
 
-    protected String createQuestion(){
+    protected String createQuestion() {
         operator = "X";
         System.out.println(firstnum + operator + secondnum);
         return createQuestion();
     }
 
-    protected int[] createChoices(){
+    protected int[] createChoices() {
         int a, b, c;
         a = firstnum * secondnum;
-        b = firstnum - (int)(5 * Math.random());
-        c = firstnum + (int)(5 * Math.random());
-        int[] result= {a, b, c};
-        /*int randomChoice = (int)(3 * Math.random());
-        switch(randomChoice){
-            case 0: result = [a, b, c];
-            break;
-            case 1: result = [b, a c];
-            break;
-            case 2: result = [c, a, b];
-            break;
-        }*/
-        return createChoices();
+        b = firstnum - (int) (3 * Math.random());
+        c = firstnum + (int) (3 * Math.random());
+        int[] result = {a, b, c};
+        int[] result_one = {b, c, a};
+        int[] result_two = {c, a, b};
+            int randomChoice = (int) (3 * Math.random());
+            switch (randomChoice) {
+                case 0: return result;
+                case 1: return result_one;
+                case 2: return result_two;
+            }
+            return createChoices();
+        }
     }
-}
+
 

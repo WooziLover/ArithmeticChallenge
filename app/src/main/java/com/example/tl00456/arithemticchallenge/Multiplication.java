bac9d8592bf747fd09c9ff6ded6fe8a4b4b8a493
WooziLover <oohty15@gmail.com>
2018-09-24 21:07:46 -0700

    Lab 4 Final

--- app/src/main/java/com/example/tl00456/arithemticchallenge/Multiplication.java
+++ app/src/main/java/com/example/tl00456/arithemticchallenge/Multiplication.java
@@ -1,17 +1,35 @@
 package com.example.tl00456.arithemticchallenge;
 
+/*************************************************
+ * Description: For the createQuestion method I wanted to create the two numbers that were random but in the range of 1-12.
+ * I wanted the Math.random() to go to 12 and start at 1 is because that is the 12 by 12 table that every kid
+ * had to learn in elementary school. I wanted to then add another variable (question) to cleanly put the three
+ * methods (firstnum, secondnum, and operator) together.
+ * Here are the reasons for createChoices. For a, I wanted to make it the correct answer and that's
+ * why I based the answers of b and c off a. Why I chose 3 for what b and c would be is because I didn't
+ * want the answer to be too big but not too small. I experimented and found 3 to be a good number. I chose to
+ * do a if statement because to me, it made sense to make a if statement. If these numbers are equal to each
+ * other, then do this specific action to fix it (multiply or divide by 2). I chose 2 because I didn't want
+ * the number again to be too small or big. The reason for why I created so many arrays and did a switch(randomChoice)
+ * was because usually games don't have the correct answer in the same spot (button) the whole time. Otherwise the
+ * player would choose the middle button (for example) every time and wouldn't learn. I immediately thought of
+ * the switch(randomChoice) because the computer will randomly choose a answer (one of the answer orders).
+ * That was my thinking process and why I decided to program it this way.
+ *
+ * Author: Tristyn Lai
+ * Date: 9/24/18 8:39 pm
+ ************************************************/
 public class Multiplication extends ArithmeticChallenge {
     int firstnum, secondnum;
-    String operator;
 
-    public Multiplication(int TOTAL_LEVELS, int TURNS_PER_LEVEL) {
-        super(TOTAL_LEVELS, TURNS_PER_LEVEL);
-        firstnum = (int) (12 * Math.random());
-        secondnum = (int) (12 * Math.random());
+    public Multiplication() {
+
     }
 
     protected String createQuestion() {
-        operator = "X";
+        firstnum = (int) (12 * Math.random() + 1);
+        secondnum = (int) (12 * Math.random()) + 1;
+        String operator = " X ";
         String question = firstnum + operator + secondnum;
         return question;
     }
@@ -19,19 +37,23 @@ public class Multiplication extends ArithmeticChallenge {
     protected int[] createChoices() {
         int a, b, c;
         a = firstnum * secondnum ;
-        b = a - (int)(2 * Math.random());
-        c = a + (int)(2 * Math.random());
+        b = a * (int)(3 * Math.random() + 1);
+        c = a * (int)(3 * Math.random() + 1);
+        if((a == b) || (a == c) || (b == a) || (b == c) || (c == a) || (c == b))
+        {
+            b = a * 2;
+            c = a / 2;
+        }
         int[] result = {a, b, c};
         int[] result_one = {b, c, a};
         int[] result_two = {c, a, b};
-            int randomChoice = (int) (3 * Math.random());
-            switch (randomChoice) {
-                case 0: return result;
-                case 1: return result_one;
-                case 2: return result_two;
-            }
-            return createChoices();
+        int randomChoice = (int) (3 * Math.random());
+        switch (randomChoice) {
+            case 0: return result;
+            case 1: return result_one;
+            case 2: return result_two;
         }
+        return createChoices();
     }
-
+}
 
